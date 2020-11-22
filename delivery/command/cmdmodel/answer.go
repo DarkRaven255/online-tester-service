93db1d52ce3d38f3f1e2eef004a0ce0263015d02
Konrad Dulemba <konrad@dulemba.me>
2020-11-22 12:45:46 +0100

    Edit the test in the database

--- delivery/command/cmdmodel/answer.go
+++ delivery/command/cmdmodel/answer.go
@@ -1,6 +1,7 @@
 package cmdmodel
 
 type Answer struct {
+	ID      uint64 `json:"id"`
 	Answer  string `json:"answer"`
 	Correct bool   `json:"correct"`
 }
--- domain/domainmodel/question.go
+++ domain/domainmodel/question.go
@@ -13,25 +13,27 @@ type Question struct {
 	Question  string     `json:"question"`
 	Required  bool       `json:"required"`
 	TestID    uint64     `json:"-"`
-	Answers   []Answer   `json:"answers" gorm:"foreignKey:QuestionID,constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Answers   []Answer   `json:"answers" gorm:"foreignKey:QuestionID,constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 func (Question) TableName() string {
 	return "onlinetests.questions"
 }
 
-func NewQuestion(q *cmdmodel.Question) *Question {
+func NewQuestion(q *cmdmodel.Question, tID uint64) *Question {
 	return &Question{
+		ID:       q.ID,
 		Question: q.Question,
 		Required: q.Required,
-		Answers:  *NewAnswerArray(&q.Answer),
+		Answers:  *NewAnswerArray(&q.Answer, q.ID),
+		TestID:   tID,
 	}
 }
 
-func NewQuestionsArray(qArr *[]cmdmodel.Question) *[]Question {
+func NewQuestionsArray(qArr *[]cmdmodel.Question, tID uint64) *[]Question {
 	questions := []Question{}
 	for _, q := range *qArr {
-		questions = append(questions, *NewQuestion(&q))
+		questions = append(questions, *NewQuestion(&q, tID))
 	}
 
 	return &questions

059f728aa497196d3c0afb3af5043b0b33896be5
Klaudia Dulemba <klaudiadulemba255@gmail.com>
2020-11-26 23:11:12 +0100

    Initialise validator. Cleanup

--- delivery/command/cmdmodel/answer.go
+++ delivery/command/cmdmodel/answer.go
@@ -2,6 +2,6 @@ package cmdmodel
 
 type Answer struct {
 	ID      uint64 `json:"id"`
-	Answer  string `json:"answer"`
-	Correct bool   `json:"correct"`
+	Answer  string `json:"answer" validate:"required"`
+	Correct bool   `json:"correct" validate:"required"`
 }
--- delivery/command/cmdmodel/test.go
+++ delivery/command/cmdmodel/test.go
@@ -1,10 +1,9 @@
 package cmdmodel
 
 type Test struct {
-	ID             uint64     `json:"id"`
-	Title          string     `json:"title"`
-	UserID         uint64     `json:"userID"`
-	NumOfQuestions uint       `json:"numOfQuestions"`
-	Questions      []Question `json:"questions"`
-	TestCode       string     `json:"testCode,omitempty"`
+	ID                 uint64     `json:"id"`
+	Title              string     `json:"title" validate:"required"`
+	NumOfTestQuestions uint       `json:"numOfTestQuestions" validate:"required"`
+	Questions          []Question `json:"questions"`
+	Randomize          bool       `json:"randomize"`
 }

6067dafcd14ea46428732ccdab3ef3a9d16a9613
Konrad Dulemba <konrad@dulemba.me>
2020-11-26 23:11:12 +0100

    Initialise validator. Cleanup

--- delivery/command/cmdmodel/question.go
+++ delivery/command/cmdmodel/question.go
@@ -2,7 +2,7 @@ package cmdmodel
 
 type Question struct {
 	ID       uint64   `json:"id"`
-	Question string   `json:"question"`
+	Question string   `json:"question" validate:"required"`
 	Answer   []Answer `json:"answers"`
-	Required bool     `json:"required"`
+	Required bool     `json:"required" validate:"required"`
 }
--- domain/domainmodel/test.go
+++ domain/domainmodel/test.go
@@ -9,15 +9,14 @@ import (
 )
 
 type Test struct {
-	ID             uint64         `json:"id" gorm:"primary_key"`
-	CreatedAt      time.Time      `json:"-"`
-	UpdatedAt      time.Time      `json:"updatedAt"`
-	DeletedAt      gorm.DeletedAt `json:"-" sql:"index"`
-	Title          string         `json:"title"`
-	UserID         uint64         `json:"userID"`
-	NumOfQuestions uint           `json:"numOfQuestions"`
-	TestCode       string         `json:"testCode" qorm:"unique"`
-	Questions      []Question     `json:"questions" gorm:"foreignKey:TestID"`
+	ID                 uint64         `json:"id" gorm:"primary_key"`
+	CreatedAt          time.Time      `json:"-"`
+	UpdatedAt          time.Time      `json:"updatedAt"`
+	DeletedAt          gorm.DeletedAt `json:"-" sql:"index"`
+	Title              string         `json:"title"`
+	NumTestOfQuestions uint           `json:"numOfTestQuestions"`
+	TestCode           string         `json:"testCode" qorm:"unique"`
+	Questions          []Question     `json:"questions" gorm:"foreignKey:TestID"`
 }
 
 func (Test) TableName() string {
@@ -27,21 +26,18 @@ func (Test) TableName() string {
 func NewTestModel(cmd *command.TestCmd) Test {
 	tCode := utils.RandomCode(8)
 	return Test{
-		Title:          cmd.Test.Title,
-		UserID:         cmd.Test.UserID,
-		NumOfQuestions: cmd.Test.NumOfQuestions,
-		TestCode:       tCode,
-		Questions:      *NewQuestionsArray(&cmd.Test.Questions, cmd.Test.ID),
+		Title:              cmd.Test.Title,
+		NumTestOfQuestions: cmd.Test.NumOfTestQuestions,
+		TestCode:           tCode,
+		Questions:          *NewQuestionsArray(&cmd.Test.Questions, cmd.Test.ID),
 	}
 }
 
 func NewEditTestModel(cmd *command.TestCmd) Test {
 	return Test{
-		ID:             cmd.Test.ID,
-		Title:          cmd.Test.Title,
-		UserID:         cmd.Test.UserID,
-		NumOfQuestions: cmd.Test.NumOfQuestions,
-		TestCode:       cmd.Test.TestCode,
-		Questions:      *NewQuestionsArray(&cmd.Test.Questions, cmd.Test.ID),
+		ID:                 cmd.Test.ID,
+		Title:              cmd.Test.Title,
+		NumTestOfQuestions: cmd.Test.NumOfTestQuestions,
+		Questions:          *NewQuestionsArray(&cmd.Test.Questions, cmd.Test.ID),
 	}
 }

76c54b5bbc1b481bc6e10af7498856327261c064
Konrad Dulemba <konrad@dulemba.me>
2020-11-29 19:17:17 +0100

    Check test exists endpoint + Cleanup

--- delivery/models/testmodel/question.go
+++ delivery/models/testmodel/question.go
@@ -6,21 +6,3 @@ type Question struct {
 	Answers  []Answer `json:"answers"`
 	Required bool     `json:"required" validate:"required"`
 }
-
-// func newTestQuestion(q *domainmodel.Question, tID uint64) *Question {
-// 	return &Question{
-// 		ID:       q.ID,
-// 		Question: q.Question,
-// 		Answers:  *newTestAnswersArray(&q.Answers, q.ID),
-// 		// TestID:   tID,
-// 	}
-// }
-
-// func newTestQuestionsArray(qArr *[]domainmodel.Question, numOfQuestions uint, tID uint64) *[]Question {
-// 	questions := []Question{}
-// 	for _, q := range *qArr {
-// 		questions = append(questions, *newTestQuestion(&q, tID))
-// 	}
-
-// 	return &questions
-// }

ed6ba340c80885070f12bc60c0c684c35fe254b4
Klaudia Dulemba <klaudiadulemba255@gmail.com>
2020-12-01 22:55:06 +0100

    Completely separate domain model from http model

--- delivery/responses/getTestResp.go
+++ delivery/responses/getTestResp.go
@@ -1,13 +1,57 @@
 package responses
 
-import "online-tests/domain/domainmodel"
+import (
+	"online-tests/delivery/models/testmodel"
+	"online-tests/domain/domainmodel"
+)
 
 type TestModel struct {
-	Test *domainmodel.Test `json:"test"`
+	Test *testmodel.Test `json:"test"`
 }
 
 func NewTestModelResp(test *domainmodel.Test) *TestModel {
 	return &TestModel{
-		Test: test,
+		Test: newTestModel(test),
 	}
+}
+
+func newTestModel(domainTest *domainmodel.Test) *testmodel.Test {
+	return &testmodel.Test{
+		ID:        domainTest.ID,
+		Title:     domainTest.Title,
+		Questions: *newTestQuestionsArray(&domainTest.Questions, domainTest.NumTestOfQuestions, domainTest.ID),
+	}
+}
+
+func newTestQuestion(q *domainmodel.Question, tID uint64) *testmodel.Question {
+	return &testmodel.Question{
+		ID:       q.ID,
+		Question: q.Question,
+		Answers:  *newTestAnswersArray(&q.Answers, q.ID),
+	}
+}
+
+func newTestQuestionsArray(qArr *[]domainmodel.Question, numOfQuestions uint, tID uint64) *[]testmodel.Question {
+	questions := []testmodel.Question{}
+	for _, q := range *qArr {
+		questions = append(questions, *newTestQuestion(&q, tID))
+	}
+
+	return &questions
+}
+
+func newTestAnswer(a *domainmodel.Answer, qID uint64) *testmodel.Answer {
+	return &testmodel.Answer{
+		ID:     a.ID,
+		Answer: a.Answer,
+	}
+}
+
+func newTestAnswersArray(aArr *[]domainmodel.Answer, qID uint64) *[]testmodel.Answer {
+	answers := []testmodel.Answer{}
+	for _, a := range *aArr {
+		answers = append(answers, *newTestAnswer(&a, qID))
+	}
+
+	return &answers
 }

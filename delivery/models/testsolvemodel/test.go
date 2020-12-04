f103464823df4e726f59779a91851dc454a04e9b
Klaudia Dulemba <klaudiadulemba255@gmail.com>
2020-12-04 01:08:19 +0100

    Return testTime together with testSolveModel

--- delivery/models/testsolvemodel/test.go
+++ delivery/models/testsolvemodel/test.go
@@ -4,4 +4,5 @@ type Test struct {
 	ID        uint64     `json:"id" validate:"required"`
 	Title     string     `json:"title"`
 	Questions []Question `json:"questions" validate:"required"`
+	TestTime  uint       `json:"testTime"`
 }

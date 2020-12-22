cd442615af311ba015c743e54c5da89b7bcb6aed
Konrad Dulemba <konrad@dulemba.me>
2020-12-22 01:23:10 +0100

    Update validator fields

--- delivery/models/testmodel/test.go
+++ delivery/models/testmodel/test.go
@@ -5,8 +5,8 @@ import "github.com/google/uuid"
 type Test struct {
 	ID                 uuid.UUID  `json:"id"`
 	Title              string     `json:"title" validate:"required"`
-	NumOfTestQuestions uint       `json:"numOfTestQuestions" validate:"required"`
-	NumOfQuestions     uint       `json:"numOfQuestions" validate:"required"`
+	NumOfTestQuestions uint       `json:"numOfTestQuestions" validate:"required,min=1"`
+	NumOfQuestions     uint       `json:"numOfQuestions" validate:"required,min=1,gtefield=NumOfTestQuestions"`
 	Questions          []Question `json:"questions" validate:"required"`
 	Randomize          bool       `json:"randomize"`
 	TestTime           uint       `json:"testTime" validate:"required"`

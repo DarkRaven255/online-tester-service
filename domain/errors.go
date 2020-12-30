318032118982bc5f539c25d66da470c5687b9324
Konrad Dulemba <konrad@dulemba.me>
2020-12-30 23:09:21 +0100

    Do not allow to overwrite other tests questions or answers

--- domain/errors.go
+++ domain/errors.go
@@ -6,7 +6,7 @@ var (
 	ErrInternalServerError = errors.New("Internal Server Error")
 	ErrNotFound            = errors.New("Your requested Item is not found")
 	ErrRecordNotFound      = errors.New("Record not found")
-	ErrConflict            = errors.New("Your item already exist")
+	ErrConflict            = errors.New("Can not overwrite other tests questions or answers")
 	ErrBadParamInput       = errors.New("Given parameter is not valid")
 	ErrUnauthorized        = errors.New("Unauthorized")
 )
